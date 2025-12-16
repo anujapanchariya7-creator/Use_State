efa4d7f4095c965d47cec04b699fc82bfb819b36
Anujah <anujapanchariya7@gmail.com>
2025-12-16 21:42:31 +0530

    Implement counter using useState

---
+++ Assessment_State_management/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import React from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
