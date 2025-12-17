66fab67fb63c393447486ae8ba1af17dff55093b
Anujah <anujapanchariya7@gmail.com>
2025-12-17 18:48:53 +0530

    Updates: 2025-12-17T13:18:51.463Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,11 +1,14 @@
-
+import React, { useState } from 'react';
 function App()
 {
-  const [page,setPage]=useState("home")
+  const [page,setPage]=useState("home");
 
     return(
       <div>
         <button onClick={()=>setPage("home")}>Home</button>
+        <button onClick={()=>setPage("about")}>About</button>
+        <button onClick={()=>setPage("contact")}>Contact</button>
+      </div>
     )
     
 
