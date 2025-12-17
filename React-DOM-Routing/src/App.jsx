36afa3a5e7ca90b79622ad64d42d67287cdc24f2
Anujah <anujapanchariya7@gmail.com>
2025-12-17 21:54:39 +0530

    Updates: 2025-12-17T16:24:37.096Z

--- React-DOM-Routing/src/App.jsx
+++ React-DOM-Routing/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import PostDetails from "./pages/PostDetails";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
-import NotFound from "./pages/NotFound";
+import Home from "./pages/Home.jsx";
+import PostDetails from "./pages/PostDetails.jsx";
+import About from "./pages/About.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Navbar from "./components/Navbar.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";   
 
 
 export default function App() {
