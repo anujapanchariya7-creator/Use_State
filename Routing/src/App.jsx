ff9575f1d4d9952f9cfcef5c1fd78e57cb464c9d
Anujah <anujapanchariya7@gmail.com>
2025-12-17 18:52:59 +0530

    Updates: 2025-12-17T13:22:57.272Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -8,8 +8,10 @@ function App()
         <button onClick={()=>setPage("home")}>Home</button>
         <button onClick={()=>setPage("about")}>About</button>
         <button onClick={()=>setPage("contact")}>Contact</button>
-     
+    
       {page==="home" && <h1>Welcome to the Home Page</h1>}
+      {page==="about" && <h1>Welcome to the About Page</h1>}
+      {page==="contact" && <h1>Welcome to the Contact Page</h1>}
       </div>
     )
     
