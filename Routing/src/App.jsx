08f9c416f8ea8a7246ca7e5c8628646136a1f6d9
Anujah <anujapanchariya7@gmail.com>
2025-12-17 19:49:33 +0530

    Updates: 2025-12-17T14:19:31.138Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -10,8 +10,8 @@ function App()
     <Routes>
       <Route path="/home" element={<Home/>} />
       <Route path="/about" element={<About/>} />
-      
       <Route path="/contact" element={<Contact/>} />
+      <Route path="*" element={<h2>404 Not Found</h2>} />
     </Routes>
     
     </BrowserRouter>
