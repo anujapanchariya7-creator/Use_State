956e5770f6d2b9ca6b8ecc24a35e5e40554c43f1
Anujah <anujapanchariya7@gmail.com>
2025-12-17 21:13:55 +0530

    Updates: 2025-12-17T15:43:53.443Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,64 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import PostDetails from "./pages/PostDetails";
-import About from "./pages/About";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
 
+function App()
+{
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="*" element={<h2>404 Not Found</h2>} />
+    </Routes>
+    
+    </BrowserRouter>
+  )
 
-export default function App() {
-return (
-<BrowserRouter>
-<Navbar />
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/posts/:id" element={<PostDetails />} />
-<Route path="/about" element={<About />} />
-<Route path="/login" element={<Login />} />
-
-
-{/* Protected Route */}
-<Route
-path="/dashboard"
-element={
-<ProtectedRoute>
-<Dashboard />
-</ProtectedRoute>
 }
-/>
-
-
-<Route path="*" element={<NotFound />} />
-</Routes>
-</BrowserRouter>
-);
-}
-
-
-// components/Navbar.jsx
-import { Link } from "react-router-dom";
-
-
-export default function Navbar() {
-return (
-<nav style={{ display: "flex", gap: "20px" }}>
-<Link to="/">Home</Link>
-<Link to="/about">About</Link>
-<Link to="/dashboard">Dashboard</Link>
-<Link to="/login">Login</Link>
-</nav>
-);
-}
-
-
-// components/ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
-
-
-export default function ProtectedRoute({ children }) {
-const isAuth = localStorage.getItem("isAuth");
-return isAuth ? children : <Navigate to="/login" />;
-}
+export default App;
