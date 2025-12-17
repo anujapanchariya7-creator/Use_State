e6323dd0a93e7302565edd1e98c79f27bb5f693c
Anujah <anujapanchariya7@gmail.com>
2025-12-17 19:33:01 +0530

    Updates: 2025-12-17T14:02:58.829Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -4,7 +4,9 @@ function App()
   return (
     <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
     </Routes>
     
     </BrowserRouter>
