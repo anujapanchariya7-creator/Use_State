6577e4f5c2368759faf13cc9c7b7eecefe80f293
Anujah <anujapanchariya7@gmail.com>
2025-12-17 21:19:54 +0530

    Updates: 2025-12-17T15:49:51.891Z

--- React-DOM-Routing/App.jsx
+++ React-DOM-Routing/App.jsx
@@ -8,18 +8,31 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
 
+
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts/:id" element={<PostDetails />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+return (
+<BrowserRouter>
+<Navbar />
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/posts/:id" element={<PostDetails />} />
+<Route path="/about" element={<About />} />
+<Route path="/login" element={<Login />} />
+
+
+{/* Protected Route */}
+<Route
+path="/dashboard"
+element={
+<ProtectedRoute>
+<Dashboard />
+</ProtectedRoute>
+}
+/>
+
+
+<Route path="*" element={<NotFound />} />
+</Routes>
+</BrowserRouter>
+);
 }
