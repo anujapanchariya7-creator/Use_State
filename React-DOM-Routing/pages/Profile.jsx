b8600ad88df7948949946bc3fa0ad9a68892da0e
Anujah <anujapanchariya7@gmail.com>
2025-12-17 21:22:27 +0530

    Updates: 2025-12-17T15:52:25.385Z

--- React-DOM-Routing/pages/Profile.jsx
+++ React-DOM-Routing/pages/Profile.jsx
@@ -3,13 +3,8 @@ return <h3>User Profile</h3>;
 }
 
 
-// pages/Settings.jsx
-export function Settings() {
-return <h3>Settings Page</h3>;
-}
 
-
-// pages/NotFound.jsx
+// pages/
 export default function NotFound() {
 return <h2>404 - Page Not Found</h2>;
 }
---
+++ React-DOM-Routing/pages/Settings.jsx
@@ -0,0 +1,3 @@
+export function Settings() {
+return <h3>Settings Page</h3>;
+}
