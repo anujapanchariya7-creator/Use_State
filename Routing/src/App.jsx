100655a83130c2b3d8aea7028c430b6915da2dcf
Anujah <anujapanchariya7@gmail.com>
2025-12-17 21:48:21 +0530

    Updates: 2025-12-17T16:18:19.665Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../Home.jsx';
+import Home from './Home.jsx';
 import About from './About.jsx';
 import Contact from './Contact.jsx';
 
