af3c5cde9b0e09f009ab3ce3e07b8832c35c64a4
Anujah <anujapanchariya7@gmail.com>
2025-12-17 20:01:05 +0530

    Updates: 2025-12-17T14:31:03.105Z

--- Routing/src/Home.jsx
+++ Routing/src/Home.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 function Home() {
     return(
         <>
         <nav>
             <ul>
-             
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li> 
             </ul>
         </nav>
         
