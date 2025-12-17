7c3756dc3ecb985f53ffc04ed9c52012b4a18d3b
Anujah <anujapanchariya7@gmail.com>
2025-12-17 19:04:23 +0530

    Updates: 2025-12-17T13:34:21.161Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -5,16 +5,7 @@ function App()
   const [page,setPage]=useState("home");
 
     return(
-    /*  <div>
-        <button onClick={()=>setPage("home")}>Home</button>
-        <button onClick={()=>setPage("about")}>About</button>
-        <button onClick={()=>setPage("contact")}>Contact</button>
-    
-      {page==="home" && <h1>Welcome to the Home Page</h1>}
-      {page==="about" && <h1>Welcome to the About Page</h1>}
-      {page==="contact" && <h1>Welcome to the Contact Page</h1>}
-      </div>
-*/
+   
     )
 
 }
