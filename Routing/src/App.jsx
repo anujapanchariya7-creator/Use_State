190c1b2cc43b423aab8a1940f6d4d8c2198e0f20
Anujah <anujapanchariya7@gmail.com>
2025-12-17 19:30:46 +0530

    Updates: 2025-12-17T14:00:43.551Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,12 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 function App()
 {
-
-    return(
-        <div>
-            <h1>Welcome to the Routing App</h1>
-        </div>
-    )
+  return ()
 
 }
 export default App;
