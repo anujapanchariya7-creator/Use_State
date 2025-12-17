4aa08a12ce6ac78e1edff34067b18e78173d3e70
Anujah <anujapanchariya7@gmail.com>
2025-12-17 20:05:41 +0530

    Updates: 2025-12-17T14:35:39.188Z

--- Routing/src/About.jsx
+++ Routing/src/About.jsx
@@ -1,6 +1,14 @@
 function About() {
-    
+
     return(
+        <>
+        <nav>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li> 
+            </ul>
+        </nav>
         <h2>This is About Page</h2>
     )
     
