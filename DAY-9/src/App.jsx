ba37c61742688edbd6717b8dadcf25dd0d89f1ff
Anujah <anujapanchariya7@gmail.com>
2025-12-18 19:53:37 +0530

    Updates: 2025-12-18T14:23:34.794Z

--- DAY-9/src/App.jsx
+++ DAY-9/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Login from "./login";
-import Dashboard from "./Dashboard";
-import ProtectRoute from "./ProtectRoute";
+import Home from "./Home.jsx";
+import Login from "./login.jsx";
+import Dashboard from "./Dashboard.jsx";
+import ProtectRoute from "./ProtectRoute.jsx";
 function App() {
 
   return (
