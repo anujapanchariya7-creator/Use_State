cc041c67f33b8567e79604b9e4c548b9a5db29d7
Anujah <anujapanchariya7@gmail.com>
2025-12-18 18:48:21 +0530

    Updates: 2025-12-18T13:18:19.193Z

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home.jsx';
-import About from './About.jsx';
-import Contact from './Contact.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Contact from "./Contact.jsx";
 
-function App()
-{
+function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/home" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="*" element={<h2>404 Not Found</h2>} />
-    </Routes>
-    
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h2>404 Not Found</h2>} />
+      </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 export default App;
