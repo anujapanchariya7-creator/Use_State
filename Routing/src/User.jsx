f7910760031b0d3dd1e35ea495cc69a956c32850
Anujah <anujapanchariya7@gmail.com>
2025-12-18 19:01:40 +0530

    Updates: 2025-12-18T13:31:38.333Z

--- Routing/src/User.jsx
+++ Routing/src/User.jsx
@@ -1,20 +1,17 @@
 import BrowserRouter from "react-router-dom";
+import { link } from "react-router-dom";
 function User(){
-    const {id}=useParams();
-
-  return(
-        <>
-        <nav>
-            <ul>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li> 
-                <li><Link to="/user">User</Link></li>
-            </ul>
-        </nav>
-
-        <h2>This is Contact Page</h2>
-        </>
-    )
+    const userId = useParams();
+<>
+<nav>
+    <li>
+        <link to =" user/1">user 1</link>
+        <link to =" user/2">user 2</link>
+        <link to =" user/3">user 3</link>
+    </li>
+    <h2>User Details</h2>
+    <p>This is user details page</p>
+</nav>
+</>
 }
 export default User;
