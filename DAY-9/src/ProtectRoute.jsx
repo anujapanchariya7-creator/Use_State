a7e6205396ed4aa004468154e64d83d0e9580d74
Anujah <anujapanchariya7@gmail.com>
2025-12-18 19:29:11 +0530

    Updates: 2025-12-18T13:59:08.632Z

--- DAY-9/src/ProtectRoute.jsx
+++ DAY-9/src/ProtectRoute.jsx
@@ -1,6 +1,8 @@
-function ProtectRoute() {
-
-
-    return isAuthenticated();
+function ProtectRoute(children) {
+    if (isAuthenticated()) {
+        return children;
+    } else {
+        return <h2>Please login to access this page</h2>;
+    }
 }
 export default ProtectRoute;
