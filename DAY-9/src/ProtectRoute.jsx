943f75b638ddbd3c88cabd3202a1d555ef0da36b
Anujah <anujapanchariya7@gmail.com>
2025-12-18 19:33:03 +0530

    Updates: 2025-12-18T14:03:01.552Z

--- DAY-9/src/ProtectRoute.jsx
+++ DAY-9/src/ProtectRoute.jsx
@@ -1,11 +1,9 @@
 import { isAuthenticated } from "./Auth";
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function ProtectRoute(children) {
     if (isAuthenticated()) {
-        return <Navigate to =" /login"/>;
-    } else {
-        return <h2>Please login to access this page</h2>;
+        return <Navigate to="/login" />;
     }
 }
 export default ProtectRoute;
