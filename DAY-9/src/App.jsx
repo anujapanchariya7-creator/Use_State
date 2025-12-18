90b689ba197e9d8e4385a3f7c59e8d2a19179e15
Anujah <anujapanchariya7@gmail.com>
2025-12-18 19:41:20 +0530

    Updates: 2025-12-18T14:11:18.252Z

--- DAY-9/src/App.jsx
+++ DAY-9/src/App.jsx
@@ -4,10 +4,7 @@ import Dashboard from "./Dashboard";
 function App() {
   return (
   <BrowserRouter>
-  <nav> 
-     <link to ="/home"> Home</link>
-     <link to ="/dashboard"> Dashboard</link>
-  </nav>
+  
 
 
   </BrowserRouter>
