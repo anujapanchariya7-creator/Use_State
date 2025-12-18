bbe0687b7db330a6fac6d67a4115e9d148b90c87
Anujah <anujapanchariya7@gmail.com>
2025-12-18 18:52:43 +0530

    Updates: 2025-12-18T13:22:40.582Z

--- Routing/src/User.jsx
+++ Routing/src/User.jsx
@@ -2,8 +2,11 @@ function User(){
 <>
 <nav>
     <li>
-    <link to =" user/1">user 1</link>
+        <link to =" user/1">user 1</link>
+        <link to =" user/2">user 2</link>
+        <link to =" user/3">user 3</link>
     </li>
+    <>h2</>
 </nav>
 }
 export default User;
