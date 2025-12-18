a851f7a81a3b4c44af708e65098d05affe1aa9b0
Anujah <anujapanchariya7@gmail.com>
2025-12-18 19:00:31 +0530

    Updates: 2025-12-18T13:30:28.463Z

--- Routing/src/User.jsx
+++ Routing/src/User.jsx
@@ -1,16 +1,20 @@
 import BrowserRouter from "react-router-dom";
 import { link } from "react-router-dom";
 function User(){
-<>
-<nav>
-    <li>
-        <link to =" user/1">user 1</link>
-        <link to =" user/2">user 2</link>
-        <link to =" user/3">user 3</link>
-    </li>
-    <h2>User Details</h2>
-    <p>This is user details page</p>
-</nav>
-</>
+
+  return(
+        <>
+        <nav>
+            <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li> 
+                
+            </ul>
+        </nav>
+
+        <h2>This is Contact Page</h2>
+        </>
+    )
 }
 export default User;
