c37474be84712c31db9469087087024700290206
Anujah <anujapanchariya7@gmail.com>
2025-12-19 21:59:26 +0530

    Updates: 2025-12-19T16:29:23.777Z

--- Props_drilling/src/components/Component2.jsx
+++ Props_drilling/src/components/Component2.jsx
@@ -4,7 +4,6 @@ function Component2(props) {
   return (
     <div>
       <h3>Component 2</h3>
-      {/* Props are forwarded but NOT used */}
       <Component3 a={props.a} b={props.b} c={props.c} d={props.d} e={props.e} f={props.f} />
     </div>
   );
