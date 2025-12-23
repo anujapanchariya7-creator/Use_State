be2e2e48da13fdd2cbed0544fe9b6806cd58ee54
Anujah <anujapanchariya7@gmail.com>
2025-12-23 20:13:16 +0530

    Updates: 2025-12-23T14:43:13.778Z

--- DAY-9/src/PerformanceComponent.jsx
+++ DAY-9/src/PerformanceComponent.jsx
@@ -13,7 +13,7 @@ setNumber(Number(e.target.value));
 let t=0;
 for(let i=num;i<num;i++)
 { 
-   num +=i;
+   t +=i;
    console.log(i);
 }
 
@@ -28,7 +28,7 @@ return(
 
             <button onClick={toggleColor}>Toggle</button>
             <p> Enter  number :{number }</p>
-            <p>loop result :{num}</p>
+            <p>loop result :{t}</p>
     </div>
 )
 
