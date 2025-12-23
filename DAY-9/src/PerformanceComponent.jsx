0c81fc0f84788f6e02f80d92ab2e0c5f0b836440
Anujah <anujapanchariya7@gmail.com>
2025-12-23 19:57:15 +0530

    Updates: 2025-12-23T14:27:11.437Z

--- DAY-9/src/PerformanceComponent.jsx
+++ DAY-9/src/PerformanceComponent.jsx
@@ -8,7 +8,21 @@ function toggleColor(){
     setBgColor(bgColor==='green'?'lightgreen':'green');
 
 }
-function handleChange
+function handleChange(e){
+
+
+}
+return(
+    <div style={{padding:'20px'}}>
+        <div style={{backgroundColor: bgColor,padding:"20px",width:"220px",
+            borderRadius
+        }}></div>
+
+
+
+
+    </div>
+)
 
 }
 export default Loop;
