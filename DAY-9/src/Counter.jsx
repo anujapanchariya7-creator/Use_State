ac68047a3b661893f307d025b31e52830bbcfb10
Anujah <anujapanchariya7@gmail.com>
2025-12-23 19:12:57 +0530

    Updates: 2025-12-23T13:42:54.726Z

---
+++ DAY-9/src/Counter.jsx
@@ -0,0 +1,18 @@
+function Counter(){
+    const [counterOne,setCounterOne] =useState(0)
+    const [counterTwo,setCounterTwo] =useState(0)
+    const incrementOne=()=>{
+        setCounterOne(counterOne+1)
+    }
+    const incrementTwo=()=>{
+        setCounterTwo(counterTwo+2)
+    }
+
+    return(
+    <div> 
+    <div><button onClick={incrementOne}> Counter One -{counterOne}</button></div>
+    <div><button onClick={incrementTwo}> Counter Two -{counterTwo}</button></div>
+    </div>
+    )
+}
+export default Counter;
