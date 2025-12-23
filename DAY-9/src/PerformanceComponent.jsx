84b38a8168c4d87c853b5f84dd25890170d86ed2
Anujah <anujapanchariya7@gmail.com>
2025-12-23 20:29:56 +0530

    Updates: 2025-12-23T14:59:54.283Z

--- DAY-9/src/PerformanceComponent.jsx
+++ DAY-9/src/PerformanceComponent.jsx
@@ -1,40 +1,52 @@
-import { useMemo,useState } from "react";
+import { useState, useMemo } from "react";
 
-function Loop(){
+function Loop() {
+  const [bgColor, setBgColor] = useState("green");
+  const [number, setNumber] = useState(0);
 
-const [bgColor,setBgColor]=useState("green");//bgColor
-const [number,setNumber]=useState(0);
-function toggleColor(){
-    setBgColor(bgColor==='green'?'red':'green');
+  function toggleColor() {
+    setBgColor(bgColor === "green" ? "red" : "green");
+  }
 
-}
-function handleChange(e){
-setNumber(Number(e.target.value));
-}
+  function handleChange(e) {
+    setNumber(Number(e.target.value));
+  }
 
-const t = useMemo(()=>
-    s+=i;
-    for(let i=number;i<10000;i++)
-    { 
-    s+i;
+  const t = useMemo(() => {
     console.log(i);
-    },[number]);
+    let s = 0;
 
+    for (let i = number; i < 10000; i++) {
+      s += i;
+    }
 
+    return s;
+  }, [number]);
 
-return(
-    <div style={{padding:'20px'}}>
-        <div style={{backgroundColor: bgColor,padding:"20px",width:"220px",
-            borderRadius:"20px" }}>
-                <input type="number" placeholder="Enter number" value={number}
-                onChange={handleChange}/>
-            </div>
+  return (
+    <div style={{ padding: "20px" }}>
+      <div
+        style={{
+          backgroundColor: bgColor,
+          padding: "20px",
+          width: "220px",
+          borderRadius: "20px",
+        }}
+      >
+        <input
+          type="number"
+          placeholder="Enter number"
+          value={number}
+          onChange={handleChange}
+        />
+      </div>
 
-            <button onClick={toggleColor}>Toggle</button>
-            <p> Enter  number :{number }</p>
-            <p>loop result :{t}</p>
+      <button onClick={toggleColor}>Toggle</button>
+
+      <p>Entered number: {number}</p>
+      <p>Loop result: {t}</p>
     </div>
-)
-
+  );
 }
+
 export default Loop;
