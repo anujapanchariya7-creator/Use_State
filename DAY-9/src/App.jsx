38c466079795c0ae22f7c2f82dca486ac40b1f8a
Anujah <anujapanchariya7@gmail.com>
2025-12-23 19:14:37 +0530

    Updates: 2025-12-23T13:44:34.978Z

--- DAY-9/src/App.jsx
+++ DAY-9/src/App.jsx
@@ -1,42 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import ProtectRoute from "./ProtectRoute";
-import Home from "./Home";
-import Login from "./login";
-import Dashboard from "./Dashboard";
+import Counter from "./Counter";
 
-function App() {
+function App(){
   return (
-    <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-           <li>
-            <Link to="/login">Login</Link>
-          </li>
-
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectRoute>
-              <Dashboard />
-            </ProtectRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+<Counter/>
+  )
 }
-
 export default App;
