60e52d092a31d42b904582d093ca78856665a90d
Anujah <anujapanchariya7@gmail.com>
2025-12-23 19:11:20 +0530

    Updates: 2025-12-23T13:41:18.144Z

--- DAY-9/src/Counter.jsx
+++ DAY-9/src/Counter.jsx
@@ -2,13 +2,13 @@ function Counter(){
     const [counterOne,setCounterOne] =useState(0)
     const [counterTwo,setCounterTwo] =useState(0)
     const incrementOne=()=>{
-
+        setCounterOne(counterOne+1)
     }
 
     return(
     <div> 
-    <div><button onChange={incrementOne}> Counter One </button></div>
-    <div><button> Counter Two </button></div>
+    <div><button onChange={incrementOne}> Counter One -{counterOne}</button></div>
+    <div><button onC> Counter Two -{counterTwo}</button></div>
     </div>
     )
 }
