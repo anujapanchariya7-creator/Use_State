d7cd6335e394c32a85eb478e9de1849af3fe05f0
Anujah <anujapanchariya7@gmail.com>
2025-12-24 18:42:31 +0530

    Updates: 2025-12-24T13:12:28.532Z

---
+++ Performance_opti2/src/PerformanceComponent.jsx
@@ -0,0 +1,52 @@
+import { useState, useMemo } from "react";
+
+function Loop() {
+  const [bgColor, setBgColor] = useState("green");
+  const [number, setNumber] = useState(0);
+
+  function toggleColor() {
+    setBgColor(bgColor === "green" ? "red" : "green");
+  }
+
+  function handleChange(e) {
+    setNumber(Number(e.target.value));
+  }
+
+  const t = useMemo(() => {
+console.log("Loop running for number:", number);
+    let s = 0;
+
+    for (let i = number; i < 10000; i++) {
+      s += i;
+    }
+
+    return s;
+  }, [number]);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <div
+        style={{
+          backgroundColor: bgColor,
+          padding: "20px",
+          width: "220px",
+          borderRadius: "20px",
+        }}
+      >
+        <input
+          type="number"
+          placeholder="Enter number"
+          value={number}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button onClick={toggleColor}>Toggle</button>
+
+      <p>Entered number: {number}</p>
+      <p>Loop result: {t}</p>
+    </div>
+  );
+}
+
+export default Loop;
