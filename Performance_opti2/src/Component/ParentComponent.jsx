e1dd69c2546637b76433718db9fe6ea89b470a49
Anujah <anujapanchariya7@gmail.com>
2025-12-24 19:53:15 +0530

    Updates: 2025-12-24T14:23:13.228Z

--- Performance_opti2/src/Component/ParentComponent.jsx
+++ Performance_opti2/src/Component/ParentComponent.jsx
@@ -7,12 +7,12 @@ import React from "react";
 
 function ParentComponent() {
   const [age, setAge] = useState(22);
-  const [salary, setSalary] = useState(500000);
+  const [salary, setSalary] = useState{(500000);
 
   const incrementAge = useCallback()=> {
     setAge(age + 1);
   };
-
+}
   const incrementSalary = () => {
     setSalary(salary + 5000);
   };
