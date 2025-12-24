4217abc84f39e97a1eb8ed520baefb1b0b020f2b
Anujah <anujapanchariya7@gmail.com>
2025-12-24 20:31:38 +0530

    Updates: 2025-12-24T15:01:36.388Z

--- Performance_opti2/src/App.jsx
+++ Performance_opti2/src/App.jsx
@@ -1,12 +1,14 @@
+import { Suspense } from "react";
 
 const Lazy=React.lazy(()=>import(./LazyLoad))
 function App(){
   return ( 
     <div className="App"> 
       <h2>Lazy Loading</h2>
-      <LazyLoad/>
-      
-      
+      <Suspense>
+      <></>
+
+      </Suspense>
       </div>
   )
 }
