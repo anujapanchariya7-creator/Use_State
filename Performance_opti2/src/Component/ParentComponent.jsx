f7abb5fdcf0beb7238b16e8d81b84d214da950a0
Anujah <anujapanchariya7@gmail.com>
2025-12-24 18:53:49 +0530

    Updates: 2025-12-24T13:23:47.295Z

--- Performance_opti2/src/Component/ParentComponent.jsx
+++ Performance_opti2/src/Component/ParentComponent.jsx
@@ -6,12 +6,19 @@ const [age,setAge]=useState(22);
 const [salary,setSalary]=useState(500000);
 
 const incrementAge=()={
-setAge(age+1);
+setAge(age+1)
 }
 const incrementSalary=()={
     
 }
+return(
+<div>
+<Title/>
 
+</div>
+
+
+)
 
 }
 export default ParentComponent;
