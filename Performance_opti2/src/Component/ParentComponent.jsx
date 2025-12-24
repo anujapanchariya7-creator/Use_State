ad3666ccb1146b3247771d698caf08f730f74a40
Anujah <anujapanchariya7@gmail.com>
2025-12-24 18:51:01 +0530

    Updates: 2025-12-24T13:20:59.070Z

--- Performance_opti2/src/Component/ParentComponent.jsx
+++ Performance_opti2/src/Component/ParentComponent.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 
 function ParentComponent()
 {
 const [age,setAge]=useState(22);
+const [salary,setSalary]=useState(500000);
+
 
 }
 export default ParentComponent;
