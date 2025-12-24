58c61e18682a689d01030ce6fcfabdc5e0df475f
Anujah <anujapanchariya7@gmail.com>
2025-12-24 19:30:03 +0530

    Updates: 2025-12-24T14:00:01.077Z

--- Performance_opti2/src/Component/ParentComponent.jsx
+++ Performance_opti2/src/Component/ParentComponent.jsx
@@ -3,28 +3,29 @@ import Title from "./Title";
 import Count from "./Count";
 import Button from "./Button";
 
-function ParentComponent()
-{
-const [age,setAge]=useState(22);
-const [salary,setSalary]=useState(500000);
+function ParentComponent() {
+  const [age, setAge] = useState(22);
+  const [salary, setSalary] = useState(500000);
 
-const incrementAge=()=>{
+  const incrementAge = () => {
+    setAge(age + 1);
+  };
 
-    setAge(age+1)
-}
-const incrementSalary=()=>{
-    setSalary(salary+5000)
-}
-return(
+  const incrementSalary = () => {
+    setSalary(salary + 5000);
+  };
+
+  return (
     <div>
-    <Title/>
-    <Count text="Age" count={age}/>
-    <Button handleClick={incrementAge} ></Button>
+      <Title />
 
-    <Count text="Salary" count={salary}/>
-    <Button handleClick={incrementSalary} ></Button>
+      <Count text="Age" count={age} />
+      <Button handleClick={incrementAge}>Increment Age</Button>
+
+      <Count text="Salary" count={salary} />
+      <Button handleClick={incrementSalary}>Increment Salary</Button>
     </div>
-    )
-
+  );
 }
+
 export default ParentComponent;
