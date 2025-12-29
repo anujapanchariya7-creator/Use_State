e4b56908c52aecfff17e4fbd2bf774bf83c308ce
Anujah <anujapanchariya7@gmail.com>
2025-12-29 19:53:23 +0530

    Updates: 2025-12-29T14:23:21.323Z

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -1,7 +1,7 @@
 function App(){
 return(
 <div className="bg-sky-300 h-screen w-screen">
-    <div class="bg-sky-400  px-6 py-4 text-white">
+    <div class="bg-sky-400  px-6 py-4 text-white ">
       <h1> My webpage</h1>
       <nav>
         <a href="#">Home</a>
