3a350404f23c562c3d5d7510358f3517dade444e
Anujah <anujapanchariya7@gmail.com>
2025-12-29 19:48:35 +0530

    Updates: 2025-12-29T14:18:32.524Z

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -3,7 +3,9 @@ return(
 <div className="bg-sky-300 h-screen w-screen">
     <div>
       <h1> My webpage</h1>
-      
+      <nav>
+        
+      </nav>
     </div>
     <div>Core</div>
     <div>Footer</div>
