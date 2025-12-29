1a1de05c9ba02bbf08eb1829ee3f970d7618b86d
Anujah <anujapanchariya7@gmail.com>
2025-12-29 19:35:49 +0530

    Updates: 2025-12-29T14:05:47.318Z

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -1,7 +1,7 @@
 
 function App(){
 return(
-  <div class="bg-amber-300 p-25"> 
+  <div class="bg-amber-300 p-15 m-20"> 
   <p className="text-black-500 bg-sky-500 w-full">Hello Tailwind CSS</p>
   <p>anuja</p>
 
