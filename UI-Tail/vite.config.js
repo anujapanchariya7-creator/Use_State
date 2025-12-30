3c9fe3225df472b284b20404152b87f69b6210f6
Anujah <anujapanchariya7@gmail.com>
2025-12-30 19:18:22 +0530

    Updates: 2025-12-30T13:48:19.232Z

--- UI-Tail/vite.config.js
+++ UI-Tail/vite.config.js
@@ -6,4 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
